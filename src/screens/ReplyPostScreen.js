c41c6336c1a38ececc77b8bd2942ea94b912f6eb
sendyakbar <msendyakbars@gmail.com>
2023-07-10 03:09:25 +0700

    feat: reply post

--- src/screens/ReplyPostScreen.js
+++ src/screens/ReplyPostScreen.js
@@ -1,26 +1,145 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Layout, Text} from '@ui-kitten/components';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {Button, Input, Layout, Spinner, Text} from '@ui-kitten/components';
+import {useRoute} from '@react-navigation/native';
+import {useQuery, useMutation, useQueryClient} from '@tanstack/react-query';
+import {v4 as uuidv4} from 'uuid';
+import Toast from 'react-native-toast-message';
+
+import FeedItemComponent from '../components/FeedItemComponent';
+import {queryKey} from '../utils/Constants';
+import {CreateReplies, GetReplies} from '../service/Service';
+
+const maxChar = 100;
 
 export default function ReplyPostScreen() {
-  const insets = useSafeAreaInsets();
-  const styles = styleGenerator(insets);
+  const route = useRoute();
+  const queryClient = useQueryClient();
+  const {data} = route.params;
+  const [content, setContent] = useState('');
+  const {data: replies, isInitialLoading: isLoadingReplies} = useQuery({
+    queryKey: [queryKey.REPLIES, data.id],
+    queryFn: () => GetReplies(data.id),
+    keepPreviousData: true,
+  });
+  const {mutate, isLoading} = useMutation({
+    mutationFn: CreateReplies,
+    onSuccess: event => {
+      console.log(event);
+      queryClient.invalidateQueries({queryKey: [queryKey.REPLIES]});
+      Toast.show({
+        type: 'success',
+        text1: 'Yeay!',
+        text2: event,
+      });
+    },
+  });
+
+  const onChangeReply = input => {
+    setContent(input);
+  };
+
+  const onPressReply = () => {
+    const userId = uuidv4();
+    mutate({
+      userId,
+      postId: data.id,
+      text: content,
+    });
+    setContent('');
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.replyItemContainer}>
+        <FeedItemComponent {...item} />
+      </View>
+    );
+  };
 
   return (
     <Layout style={styles.container}>
-      <Text category="h2">Hello this is Reply screen</Text>
+      <View style={styles.header}>
+        <FeedItemComponent {...data} />
+      </View>
+      {isLoadingReplies ? (
+        <Layout style={styles.loadingContainer}>
+          <Spinner size="giant" />
+        </Layout>
+      ) : (
+        <FlatList
+          data={replies}
+          renderItem={renderItem}
+          keyExtractor={(_, i) => String(i)}
+          contentContainerStyle={styles.repliesContainer}
+        />
+      )}
+      <View style={styles.footer}>
+        <Input
+          placeholder="Type your reply here..."
+          style={styles.input}
+          multiline
+          onChangeText={onChangeReply}
+          value={content}
+        />
+        <Button
+          appearance="outline"
+          size="small"
+          style={styles.button}
+          onPress={onPressReply}
+          disabled={content.length > maxChar || isLoading}>
+          {isLoading ? (
+            <Spinner size="small" status="control" />
+          ) : (
+            <Text>REPLY</Text>
+          )}
+        </Button>
+      </View>
+      <View style={styles.bottomInfo}>
+        <Text status={content.length > maxChar ? 'danger' : 'basic'}>
+          {`${content.length}/${maxChar}`}
+        </Text>
+      </View>
     </Layout>
   );
 }
 
-const styleGenerator = insets =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      // Paddings to handle safe area
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      paddingHorizontal: 16,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  replyItemContainer: {
+    paddingLeft: 32,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  repliesContainer: {
+    flexGrow: 1,
+    paddingHorizontal: 16,
+  },
+  header: {
+    paddingHorizontal: 16,
+  },
+  footer: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  input: {
+    flex: 1,
+    paddingRight: 8,
+  },
+  button: {
+    height: 39,
+  },
+  bottomInfo: {
+    backgroundColor: 'black',
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+  },
+});
