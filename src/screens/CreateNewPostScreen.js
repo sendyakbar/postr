b361da378e82611913169f5f725ec96004bd530b
sendyakbar <msendyakbars@gmail.com>
2023-07-10 03:08:44 +0700

    feat: create new post

--- src/screens/CreateNewPostScreen.js
+++ src/screens/CreateNewPostScreen.js
@@ -1,15 +1,70 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Layout, Text} from '@ui-kitten/components';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Layout, Text, Input, Button, Spinner} from '@ui-kitten/components';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {v4 as uuidv4} from 'uuid';
+import Toast from 'react-native-toast-message';
 
-export default function CreateNewPostScreen() {
+import {CreatePost} from '../service/Service';
+import {queryKey} from '../utils/Constants';
+
+const maxChar = 100;
+
+export default function CreateNewPostScreen({navigation}) {
   const insets = useSafeAreaInsets();
   const styles = styleGenerator(insets);
+  const queryClient = useQueryClient();
+  const [content, setContent] = useState('');
+  const {mutate, isLoading} = useMutation({
+    mutationFn: CreatePost,
+    onSuccess: event => {
+      queryClient.invalidateQueries({queryKey: [queryKey.POSTS]});
+      Toast.show({
+        type: 'success',
+        text1: 'Yeay!',
+        text2: event,
+      });
+      navigation.goBack();
+    },
+  });
+
+  const onChangeContent = input => {
+    setContent(input);
+  };
+
+  const onPressPost = () => {
+    const userId = uuidv4();
+    mutate({userId, text: content});
+  };
 
   return (
     <Layout style={styles.container}>
-      <Text category="h2">Hello this is Create New Post Screen</Text>
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Type your post here..."
+          multiline
+          onChangeText={onChangeContent}
+          value={content}
+        />
+        <Text
+          status={content.length > maxChar ? 'danger' : 'basic'}
+          style={styles.charCount}>
+          {`${content.length}/${maxChar}`}
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <Button
+          appearance="outline"
+          disabled={content.length > maxChar || isLoading}
+          onPress={onPressPost}>
+          {isLoading ? (
+            <Spinner size="small" status="control" />
+          ) : (
+            <Text>POST</Text>
+          )}
+        </Button>
+      </View>
     </Layout>
   );
 }
@@ -22,5 +77,14 @@ const styleGenerator = insets =>
       paddingTop: insets.top,
       paddingBottom: insets.bottom,
       paddingHorizontal: 16,
+    },
+    inputContainer: {
+      flex: 1,
+    },
+    footer: {
+      paddingVertical: 16,
+    },
+    charCount: {
+      marginTop: 4,
     },
   });
