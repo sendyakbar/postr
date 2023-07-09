b3a79613afd0c2dd4dab950b8512ce301355d065
sendyakbar <msendyakbars@gmail.com>
2023-07-10 04:45:29 +0700

    init: data geolocation

--- src/data/index.js
+++ src/data/index.js
@@ -4,12 +4,20 @@ const posts = [
     userId: 'b8057b5e-089e-497e-b2ce-965ee556f4b2',
     text: 'Halo-halo bandung, ibukota periangan. Halo-halo bandung, ibukota periangan',
     createdAt: new Date(),
+    geoLocation: {
+      latitude: 37.4226711,
+      longitude: -122.0849872,
+    },
   },
   {
     id: 'd5bca7d9-0c5c-4f1f-b315-117e53d8414d',
     userId: '91b4f06b-19a5-401b-9f23-998276441492',
     text: 'Sudah lama beta tidak berjumpa dengan kau. Sudah lama beta tidak berjumpa dengan kau',
     createdAt: new Date(),
+    geoLocation: {
+      latitude: 37.4226711,
+      longitude: -122.0849872,
+    },
   },
 ];
 
@@ -20,6 +28,10 @@ const replies = [
     userId: 'b8057b5e-089e-497e-b2ce-965ee556f4b2',
     text: 'ah masa sih?',
     createdAt: new Date(),
+    geoLocation: {
+      latitude: 37.4226711,
+      longitude: -122.0849872,
+    },
   },
   {
     id: 'ea291375-362e-4aac-b2c2-428e28ca487e',
@@ -27,6 +39,10 @@ const replies = [
     userId: '91b4f06b-19a5-401b-9f23-998276441492',
     text: 'oooh begituu...',
     createdAt: new Date(),
+    geoLocation: {
+      latitude: 37.4226711,
+      longitude: -122.0849872,
+    },
   },
 ];
 
