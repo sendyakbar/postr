1417638e1ec6c107e85bcb6e9f953afde9acfa77
sendyakbar <msendyakbars@gmail.com>
2023-07-10 03:07:33 +0700

    setup: react-query in App.js

--- App.js
+++ App.js
@@ -4,23 +4,28 @@ import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider} from '@ui-kitten/components';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
 
 import RootNavigator from './src/navigation/RootNavigator';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.dark}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-          />
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </ApplicationProvider>
+    <QueryClientProvider client={queryClient}>
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <StatusBar
+              barStyle="light-content"
+              backgroundColor="transparent"
+              translucent
+            />
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ApplicationProvider>
+    </QueryClientProvider>
   );
 }
 
