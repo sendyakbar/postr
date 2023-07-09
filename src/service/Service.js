e37492947e86693d0940678ca1e280f96065e0db
sendyakbar <msendyakbars@gmail.com>
2023-07-10 03:09:00 +0700

    init: service functions

---
+++ src/service/Service.js
@@ -0,0 +1,64 @@
+import {posts, replies} from '../data';
+import {v4 as uuidv4} from 'uuid';
+
+export const GetPosts = () => {
+  return new Promise((resolve, reject) => {
+    if (!posts) {
+      reject(new Error('No post available'));
+    }
+    setTimeout(() => {
+      resolve(posts);
+    }, 1000);
+  });
+};
+
+export const CreatePost = ({userId, text}) => {
+  return new Promise((resolve, reject) => {
+    const post = {
+      id: uuidv4(),
+      userId,
+      text,
+      createdAt: new Date(),
+    };
+    if (text.length > 100) {
+      reject('Maximum 100 characters');
+    }
+    posts.unshift(post);
+    setTimeout(() => {
+      resolve('New post created');
+    }, 1000);
+  });
+};
+
+export const GetReplies = postId => {
+  return new Promise(resolve => {
+    const res = replies.filter(d => d.postId === postId);
+    if (!res.length) {
+      setTimeout(() => {
+        resolve([]);
+      }, 1200);
+    }
+    setTimeout(() => {
+      resolve(res);
+    }, 2000);
+  });
+};
+
+export const CreateReplies = ({userId, postId, text}) => {
+  return new Promise((resolve, reject) => {
+    const post = {
+      id: uuidv4(),
+      postId,
+      userId,
+      text,
+      createdAt: new Date(),
+    };
+    if (text.length > 100) {
+      reject('Maximum 100 characters');
+    }
+    replies.unshift(post);
+    setTimeout(() => {
+      resolve('New reply created');
+    }, 1500);
+  });
+};
